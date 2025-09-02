e8bc39fe2cb6597e6bbd86d4ff212592e5ebcbec
Will <Will>
2025-09-02 10:24:36 +0100

    Solved Euler problem 3

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-current = 3
-sum = 2
-prev = 2
+#list prime factors of inputted number
 
-while current <= 4000000:
-    temp = current
-    current = current + prev
-    prev = temp
-    if current % 2 == 0:
-        sum += current
-print(sum)
+def findFactor(num):
+    factors = []
+    factor = 2
+    while num >=2:
+        if num % factor == 0:
+            factors.append(factor)
+            num = num // factor
+        else:
+            factor+=1
+    factors = list(set(factors))
+    return factors
+
+print(findFactor(int(input("Number to be prime factored"))))
